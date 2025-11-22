e42fd5d432d1a3ee311a2ddba11676d9aa7777c5
Mohammadibbu <mohammadibbu@gmail.com>
2025-11-22 10:50:14 +0530

    update crypto.ts

--- FRONTEND/src/utils/crypto.ts
+++ FRONTEND/src/utils/crypto.ts
@@ -1,7 +1,6 @@
 // utils/crypto.ts
 import CryptoJS from "crypto-js";
-
-const SECRET_KEY = "YOUR_SECRET_KEY_32_CHARS";
+const SECRET_KEY = "K8h2ZxP1vQm9T4bR7cF0sLwE3nYgU5a";
 
 export const encryptData = (data: any) => {
   return CryptoJS.AES.encrypt(JSON.stringify(data), SECRET_KEY).toString();
